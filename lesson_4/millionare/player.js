5529bc6b9e872fb23743c19650d46c156c9983a1
Pavel <stodumoff@yandex.ru>
2021-11-16 14:46:44 +0300

    task 4 is compleate

--- lesson_4/millionare/player.js
+++ lesson_4/millionare/player.js
@@ -1,19 +1,63 @@
 'use strict'
 
-let player = {
-  playerAnswer() {
-    
-  },
+class Player {
+  constructor(points) {
+    this.points = points
+  }
+  showPoints() {
+    console.log(`${'$'.repeat(10)} Ваш баланс: ${this.points} ${'$'.repeat(10)}В`)
+  }
+  addPoints() {
+    this.points += 200000
+  }
   getPlayerAnswer(answer) {
     const letters = ['a', 'b', 'c', 'd']
-    while (true){
-      answer = prompt('Введите букву вашего ответа:').toLowerCase()
-      
-      if (letters.includes(answer)) {
-        return answer
-      }else {
+    while (true) {
+      answer = prompt('Введите букву вашего ответа, или "Отмена" для выхода')
+      if (answer == null) {
+        alert('До свидания')
+        console.log('Вы вышли из игры')
+        return null
+      }
+      if (letters.includes(answer.toLowerCase())) {
+        return answer.toUpperCase()
+      } else {
         alert('Введите букву вашего варианта ответа')
       }
     }
   }
 }
+
+let player = new Player(0)
+
+
+
+
+
+
+
+
+
+
+
+// let player = {
+//   playerPoints() {
+//     let points = 0
+//   },
+//   getPlayerAnswer(answer) {
+//     const letters = ['a', 'b', 'c', 'd']
+//     while (true) {
+//       answer = prompt('Введите букву вашего ответа')
+//       if (answer == null) {
+//         alert('До свидания')
+//         console.log('Вы вышли из игры')
+//         return null
+//       }
+//       if (letters.includes(answer.toLowerCase())) {
+//         return answer.toUpperCase()
+//       } else {
+//         alert('Введите букву вашего варианта ответа')
+//       }
+//     }
+//   }
+// }
--- lesson_4/millionare/tasks.js
+++ lesson_4/millionare/tasks.js
@@ -1,12 +1,24 @@
 let task = {
   getTasks() {
-    for (let question of questions) {
-      question.showQuestion()
+    do {
+      console.clear()
+      for (let question of questions) {
+        question.showQuestion()
+        question.showVariants()
+        if (question.checkAnswer(player.getPlayerAnswer())) {
+          player.showPoints()
+          if (confirm('Играем дальше?') == true) {
+            console.clear()
+            continue
+          } else {
+            break
+          }
+        } else {
+          break
+        }
       }
-      //player.getPlayerAnswer()
-      
-    }
-      
-    
+      alert(`Конец игры! Ваш баланс ${player.points}`)
+    } while (confirm('Играть ещё?') == true)
   }
-    
+}
+
