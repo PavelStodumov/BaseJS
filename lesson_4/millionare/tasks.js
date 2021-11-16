bfeb2a038004e3c873261758d104a72f22fc1f72
Pavel <stodumoff@yandex.ru>
2021-11-16 16:09:11 +0300

    womework 4 upgreade

--- lesson_4/millionare/tasks.js
+++ lesson_4/millionare/tasks.js
@@ -2,7 +2,9 @@ let task = {
   getTasks() {
     do {
       console.clear()
-      for (let question of questions) {
+      // for (let question of questions) {
+      while (questions.length > 0) {
+        let question = questions.splice(getRandomArbitrary(0, questions.length), 1)[0]
         question.showQuestion()
         question.showVariants()
         if (question.checkAnswer(player.getPlayerAnswer())) {
